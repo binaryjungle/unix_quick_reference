5095ffb6c4c83e59a76c57ca056968f58f94cb7e
Vaithyanathan Ramanathan <vaithy31@gmail.com>
2019-06-23 01:53:29 +0530

    Initial Commit

---
+++ pwd/pwd.sh
@@ -0,0 +1,8 @@
+#Shows present working directory
+pwd
+
+#Shows present working directory from where it was orignally navigated (useful for symbolic links)
+pwd -L
+
+#Shows present working directory it the directory is physically present (useful for symbolic links)
+pwd -P
