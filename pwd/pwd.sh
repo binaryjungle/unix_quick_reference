e8db3543ebd00ee276ecc7a13faae9d71eedd59a
Vaithyanathan Ramanathan <vaithy31@gmail.com>
2019-06-23 01:55:16 +0530

    Correcting grammatical mistake in the comments

--- pwd/pwd.sh
+++ pwd/pwd.sh
@@ -4,5 +4,5 @@ pwd
 #Shows present working directory from where it was orignally navigated (useful for symbolic links)
 pwd -L
 
-#Shows present working directory it the directory is physically present (useful for symbolic links)
+#Shows present working directory where it is physically present (useful for symbolic links)
 pwd -P
