a173d3e6b641b6e6689fc3bfe3c6bfc5b2a49d96
Vaithyanathan Ramanathan <vaithy31@gmail.com>
2020-06-05 01:25:19 +0530

    Initial commit for version and permission related commands

---
+++ permissions/permissions.sh
@@ -0,0 +1,7 @@
+chmod 750 file_name.txt
+chown tutor:staff file_name.txt
+umask 027
+chmod -R 750 /home/tutor/permissions/
+chown -R tutor:staff /home/tutor/permissions/
+chown -R tutor: /home/tutor/permissions/
+chown -R :staff /home/tutor/permissions/
---
+++ version/version.sh
@@ -0,0 +1,11 @@
+cat /etc/*-release
+ls -l /etc/*release
+
+uname
+uname -a
+uname -m
+uname -n
+uname -p
+uname -r
+uname -s
+uname -v
